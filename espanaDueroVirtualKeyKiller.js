3d33ed7cc104260bfd279aaee65f65cf15e599b6
DiCrEn <dicrenmail@gmail.com>
2018-10-30 19:47:17 +0100

    Update espanaDueroVirtualKeyKiller.js
    Actualizado a la nueva página.

--- espanaDueroVirtualKeyKiller.js
+++ espanaDueroVirtualKeyKiller.js
@@ -1,22 +1,20 @@
 // ==UserScript==
-// @name         Unicaja Kill virtual keyboard
+// @name         EspañaDuero Kill virtual keyboard
 // @namespace    https://github.com/DiCrEn/espanaDueroVirtualKeyKiller/
-// @version      0.2
-// @description  Pequeño script para el navegador para quitar el molesto teclado virtual de la web www.unicajabanco.es y poder acceder escribiendo la contraseña con el teclado físico.
+// @version      0.3
+// @description  Pequeño script para el navegador para quitar el molesto teclado virtual de la web www.espanaduero.es y poder acceder escribiendo la contraseña con el teclado físico.
 // @author       DiCrEn
-// @match        https://www.unicajabanco.es/*
+// @match        https://areaprivada.unicajabanco.es/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    if(document.querySelector("#acceso-usuario") != null){
         document.querySelector("#clave").readOnly = false; //Ahora puedo escribir en el campo clave
         document.querySelector("#clave").onfocus = null; //Ahora ya no sale el horrible teclado virtual.
-        document.querySelector("#datos").onsubmit = function(){
+        document.querySelector("form[name='datos']").onsubmit = function(){
             //Al enviar el formulario copio los valores anteriores en los campos que realmente valen.
             document.querySelector("input[name='usuario']").value = document.querySelector("#user").value;
             document.querySelector("input[name='password']").value = document.querySelector("#clave").value;
         };
-    }
 })();
